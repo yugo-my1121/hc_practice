d0671df86a634712a5746529b6e052b52dc33735
yugo <yugo1121@gmail.com>
2023-10-24 05:11:49 +0900

    juiceクラスを追加とjuiceクラスに置き換え

--- ruby/vending_machine/machine.rb
+++ ruby/vending_machine/machine.rb
@@ -1,4 +1,6 @@
 require_relative 'suica'
+require_relative 'juice'
+
 
 class Machine
 
@@ -8,22 +10,26 @@ class Machine
     def initialize
       # 初期状態で、ペプシ(150円)を5本格納している。
       @Stock_Juce = {
-        "ペプシ" =>{name:"ペプシ",price:150,quantity:5},
-        "モンスター" =>{name:"モンスター",price:230,quantity:5},
-        "いろはす" =>{name:"いろはす",price:120,quantity:5}
+        "ペプシ" =>{juice:Juice.new("ペプシ",150),quantity:5},
+        "モンスター" =>{juice:Juice.new("モンスター",230),quantity:5},
+        "いろはす" =>{juice:Juice.new("いろはす",120),quantity:5}
+
+        #  "ペプシ" =>{name:"ペプシ",price:150,quantity:5},
+        #  "モンスター" =>{name:"モンスター",price:230,quantity:5},
+        #  "いろはす" =>{name:"いろはす",price:120,quantity:5}
       }
       @sales = 0
     end
 
     # 自動販売機は在庫を取得できる
-    def Stock_Juce(name)
-      @Stock_Juce[name][:quantity]
-    end
+     def Stock_Juce(name)
+       @Stock_Juce[name][:quantity]
+     end
 
     # 自動販売機はペプシが購入できるかどうかを取得できる。
     # →suicaの残高がペプシの値段以上かつペプシの本数が0より多い時trueそれ以外false
     def Purchase_Check?(suica,name)
-      suica.balance >= @Stock_Juce[name][:price] && @Stock_Juce[name][:quantity] > 0
+      suica.balance >= @Stock_Juce[name][:juice].price && @Stock_Juce[name][:quantity] > 0
     end
 
     # ジュース値段以上のチャージ残高がある条件下で購入操作を行うと以下の動作をする
@@ -39,10 +45,10 @@ class Machine
         @Stock_Juce[name][:quantity] -= 1
         @Stock_Juce[name][:quantity]
         #売り上げ金額を増やす
-        @sales += @Stock_Juce[name][:price]
+        @sales += @Stock_Juce[name][:juice].price
 
         #Suicaのチャージ金額を減らす
-        suica.pay(@Stock_Juce[name][:price])
+        suica.pay(@Stock_Juce[name][:juice].price)
 
       else
         raise "売り切れです"
@@ -58,7 +64,7 @@ class Machine
      def buy_drink_list(suica)
       list = []
       @Stock_Juce.each do |key,value|
-        if suica.balance >= value[:price] && value[:quantity] > 0
+        if suica.balance >= value[:juice].price && value[:quantity] > 0
           list << key
         end
 
